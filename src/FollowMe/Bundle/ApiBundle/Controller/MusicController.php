5152e5476362c3590730f19b4c38043f1466c683
Mathieu PIERAGGI <matt.pieraggi@gmail.com>
2015-03-12 18:53:24 +0100

    Add Music controller and routing

---
+++ src/FollowMe/Bundle/ApiBundle/Controller/MusicController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FollowMe\Bundle\ApiBundle\Controller;
+
+use Doctrine\ORM\EntityManager;
+use FOS\RestBundle\Controller\Annotations\View as FosView;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+
+class MusicController extends SuperController
+{
+
+}
