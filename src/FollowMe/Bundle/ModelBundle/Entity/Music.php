7b0fca73fc4a673be4d14b8a960a9a9fe7356c70
Mathieu PIERAGGI <matt.pieraggi@gmail.com>
2015-03-12 18:50:46 +0100

    Add Music model

---
+++ src/FollowMe/Bundle/ModelBundle/Entity/Music.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace FollowMe\Bundle\ModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\Since;
+
+/**
+ * Music
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="FollowMe\Bundle\ModelBundle\Entity\MusicRepository")
+ *
+ * @ExclusionPolicy("NONE")
+ */
+class Music
+{
+    /**
+     * Music's identifier number
+     *
+     * @var integer
+     *
+     * @Since("0.1")
+     * @Groups({"all", "list", "info"})
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Author of the music
+     *
+     * @var string
+     *
+     * @Since("0.1")
+     * @Groups({"all", "list", "info"})
+     *
+     * @ORM\Column(name="artist", type="string", length=255)
+     */
+    private $artist;
+
+    /**
+     * Location of the music file
+     *
+     * @var string
+     *
+     * @Since("0.1")
+     * @Groups({"all", "list", "info"})
+     *
+     * @ORM\Column(name="url", type="string", length=255, unique=true)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set artist
+     *
+     * @param string $artist
+     * @return Music
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    /**
+     * Get artist
+     *
+     * @return string 
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Music
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
